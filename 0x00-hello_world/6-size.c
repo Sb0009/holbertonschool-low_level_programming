26dc0f188fda0bbfd7b1e47de93d34361a8a3a54
Siham <phenissia2009@hotmail.fr>
2022-02-03 22:04:38 +0100

    size --

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,10 +7,10 @@
 
 int main(void)
 {
-printf("Size of a char: 1 byte(s)\n", sizeof(char));
-printf("Size of an int: 4 byte(s)\n", sizeof(int));
-printf("Size of a long int: %i byte(s)\n", sizeof(long int));
-printf("Size of a long long int: 8 byte(s)\n", sizeof(long long int));
-printf("Size of a float: 4 byte(s)\n", sizeof(float));
+printf("c", sizeof(char));
+printf("i", sizeof(int));
+printf("li", sizeof(long int));
+printf("lli", sizeof(long long int));
+printf("f", sizeof(float));
 return (0);
 }
