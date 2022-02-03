fcf6ef58948cb80877c5c87b61ebefc3869313ab
Siham <phenissia2009@hotmail.fr>
2022-02-03 22:17:05 +0100

    size --

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,16 +1,9 @@
-#include <stdio.h>
-
-/**
- *main - Entry point
- *Return: Always 0 (Success)
- */
-
 int main(void)
 {
-printf("c", sizeof(char));
-printf("i", sizeof(int));
-printf("li", sizeof(long int));
-printf("lli", sizeof(long long int));
-printf("f", sizeof(float));
+printf("Size of a char: 1 byte(s)\n", sizeof(char));
+printf("Size of an int: 4 byte(s)\n", sizeof(int));
+printf("Size of a long long int: %i byte(s)\n", sizeof(long long int));
+printf("Size of a long long int: 8 byte(s)\n", sizeof(long long int));
+printf("Size of a float: 4 byte(s)\n", sizeof(float));
 return (0);
 }
