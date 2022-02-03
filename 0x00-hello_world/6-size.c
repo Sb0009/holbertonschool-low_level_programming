20d4b657882e8434a58543207c1bd60cfc6ca7cf
Siham <phenissia2009@hotmail.fr>
2022-02-03 21:11:56 +0100

    Betty style is Beautiful

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,8 +5,8 @@
  */
 int main(void)
 {
-  printf("Size of a char: 1 byte(s)\n");
- printf("Size of an int: 4 byte(s)\n")
+printf("Size of a char: 1 byte(s)\n");
+printf("Size of an int: 4 byte(s)\n")
 printf("Size of a long int: %i byte(s)\n", sizeof(long int));
 printf("Size of a long long int: 8 byte(s)\n");
 printf("Size of a float: 4 byte(s)\n"(float));
