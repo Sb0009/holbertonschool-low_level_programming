7cb7b954fa56ff3198c47ce57877205184bb1344
Siham <phenissia2009@hotmail.fr>
2022-02-03 16:06:56 +0100

    Hello, puts.

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,9 @@
 
 #include <stdio.h>
-/**
- * main - main block
- * Return: 0
- */
-int main(void)
+/*
+ *4.Hello,puts
+ */int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+  puts("\"Programming is like building a multilingual puzzle");
+  return (0);
 }
