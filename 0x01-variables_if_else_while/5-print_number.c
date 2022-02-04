0e1e252472a86f182e2d931da739f8a595b47b6e
Siham <phenissia2009@hotmail.fr>
2022-02-04 20:48:20 +0100

    task 5

---
+++ 0x01-variables_if_else_while/5-print_number.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+int c;
+for (c = 0; c <= 9; c++)
+{
+	printf("%d", c);
+}
+putchar('\n');
+return (0);
+}
