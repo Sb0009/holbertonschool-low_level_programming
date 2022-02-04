e542a71121a6be00fbab9dd16b58f22f348863ea
Siham <phenissia2009@hotmail.fr>
2022-02-04 23:19:42 +0100

    so one

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -3,16 +3,16 @@
 /**
 * main - Entry point
 *
-* Return: Easy 0 (Success)
+* Return:  0 (Success)
 */
 int main(void)
 {
 int i;
-for (i = 0; i < 89 ; i++)
+for (i = 0; i < 100 ; i++)
 {
 putchar((i / 10) + '0');
 putchar((i % 10) + '0');
-if (i < 89)
+if (i < 99)
 {
 putchar(',');
 putchar(' ');
