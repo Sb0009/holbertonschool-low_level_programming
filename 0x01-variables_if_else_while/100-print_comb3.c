c639625ce894e728dae616ce80cffc22bbfebdd4
Siham <phenissia2009@hotmail.fr>
2022-02-05 22:52:02 +0100

    suite de n

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,7 +10,7 @@ int main(void)
 int i;
 for (i = 0; i < 89 ; i++)
 {
-if ((i / 10) < (i % 10); i + 3)
+if ((i / 10) < (i % 10);)
 {
 putchar((i / 10) + '0');
 putchar((i % 10) + '0');
@@ -20,6 +20,7 @@ if (i < 89 && (i / 10) < (i % 10))
 putchar(',');
 putchar(' ');
 }
+if (i == 10 ; i +2) ; 
 }
 putchar('\n');
 return (0);
