e4f32325dfbe3dac45a0fd0e9e7c50d4be22a77f
Siham <phenissia2009@hotmail.fr>
2022-02-05 22:25:33 +0100

    Fibonacci

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,21 +8,15 @@
 
 int main()
 {
-int n, nbr1 = 0, nbr2 = 1, suivant, i;
-printf("Entrez le nombre de termes\n");
-scanf("%d", &n); 
-printf("Les %d premiers termes de la série de Fibonacci sont:\n", n); 
-for (i = 0; i < n; i++)
+int Fibonacci(int n)
 {
-if (i <= 1)
-suivant = i;
-else
+int f1 = 0;
+int f2 = 1;
+int fn;
+for ( int i = 2; i < n; i++ )
 {
-suivant = nbr1 + nbr2;
-nbr1 = nbr2;
-nbr2 = suivant;
+fn = f1 + f2;
+f1 = f2;
+f2 = fn;
 }
-printf("%d\n", suivant);
 } 
-return 0;
-}
