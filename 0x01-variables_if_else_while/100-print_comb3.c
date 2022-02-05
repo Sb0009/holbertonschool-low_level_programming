bdf4ba30d928469f0f5cfc2673bb432071e35626
Siham <phenissia2009@hotmail.fr>
2022-02-05 23:12:11 +0100

    suite

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,11 +11,11 @@ int main(void)
 int i ;
 if(i = 0 ; i < 89 ; i = i ++)
 {
-putchar("iteration %d \n", i + 1);
+putchar("iteration %d \n");
 }
 {
 else("valeur de i apres la boucle : %d \n", i + 2);
 putchar(' ');
 putchar(,);
-return (0) ;
+return (0);
 }
