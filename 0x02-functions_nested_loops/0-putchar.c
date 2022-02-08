6d1f0e2effc53394c040bcd8066ea3e0ba166f83
Siham <phenissia2009@hotmail.fr>
2022-02-08 11:34:13 +0100

    first commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* main - _putchar - program prints _putchar - Done with the ASCII
+* Return: 0
+*/
+
+int main(void)
+
+{
+_putchar(95);
+_putchar(112);
+_putchar(117);
+_putchar(116);
+_putchar(99);
+_putchar(104);
+_putchar(97);
+_putchar(114);
+_putchar('\n');
+return (0);
+}
