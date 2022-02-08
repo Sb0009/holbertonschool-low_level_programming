c4a528dfbaab1b09b5853fd5d8ff458a6f97379d
Siham <phenissia2009@hotmail.fr>
2022-02-08 22:19:05 +0100

    multiplication-table, or a mathematical equation, which, turn it how you will, balances itself

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,51 @@
+#include "main.h"
+
+/**
+ * print_times_table - prints the n times table  between 0-15
+ * @n: Integer
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errn.
+ */
+void print_times_table(int n)
+{
+int i, j, r;
+if (n >= 0 && n < 15)
+{
+for (i = 0; i <= n; i++)
+{
+for (j = 0; j <= n; j++)
+{
+k = i * j;
+if (j != 0)
+{
+_putchar (',');
+_putchar (' ');
+if (r >= 10 && r < 100)
+{
+_putchar (' ');
+_putchar (r / 10 + '0');
+_putchar (r % 10 + '0');
+}
+else if (r < 10)
+{
+_putchar (' ');
+_putchar (' ');
+_putchar (r + '0');
+}
+else
+{
+_putchar (r / 100 + '0');
+_putchar ((r / 10) % 10 + '0');
+_putchar (r % 10 + '0');
+}
+}
+else
+{
+_putchar (r  + '0');
+}
+}
+_putchar('\n');
+}
+}
+}
