a971c8640f49d57ce2fb68ce0d70991ca204669b
Siham <phenissia2009@hotmail.fr>
2022-02-08 13:56:31 +0100

    push main.h with my functions

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "man.h"
+
+/**
+ *_islower -  Run man islower to learn more..
+ *
+ * 
+ * Return: - 1 if lowercase 0 if not.
+ */
+int  _islower(int c)
+{
+if (c >= 97 && c <= 122)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
