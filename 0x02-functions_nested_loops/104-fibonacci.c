179e6ef7af900940dcebfc503882622eefa9112b
Siham <phenissia2009@hotmail.fr>
2022-02-08 23:32:02 +0100

    Art is everywhere

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,42 +8,25 @@
 int main(void)
 {
 int i;
-long int pf, af, f, f1, f2, f3;
+long int pf, af, f;
 
 pf = 1;
 af = 2;
 printf("1, ");
 printf("2, ");
-for (i = 0; i < 96; i++)
+for (i = 0; i < 48; i++)
 {
 f = pf + af;
 pf = af;
 af = f;
-if (f < 10000000)
+if (i != 47)
 {
-printf("%ld", f);
-}
-else if ((f > 10000000) && (f < 100000000000000))
-{
-f2 = f / 10000000;
-f3 = f % 10000000;
-printf("%ld", f2);
-printf("%ld", f3);
+printf("%ld, ", f);
 }
 else
 {
-f1 = f / 100000000000000;
-f2 = (f % 100000000000000) / 10000000;
-f3 = f % 10000000;
-printf("%ld", f1);
-printf("%ld, ", f2);
-printf("%ld, ", f3);
-}
-if (i != 95)
-{
-printf(", ");
+printf("%ld\n", f);
 }
 }
-printf("\n");
 return (0);
 }
