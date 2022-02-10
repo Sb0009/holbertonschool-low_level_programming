3a6710bf24f12237ae70a331f64a8e9b2135fdbf
Siham <phenissia2009@hotmail.fr>
2022-02-10 13:13:11 +0100

    ,Lancement du projet 0x04-more functions nested loops

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _isupper - check code
+ * @c: character uper only
+ * Return: Always 0.
+ */
+
+int _isupper(int c)
+{
+
+	if ((c == 65) && (c <= 90))
+	{
+		return (1);
+	}
+
+	else
+	{
+		return (0);
+	}
+}
