c1aff2a0f3e14f79effbbb3a6be390789cbd9544
Siham <phenissia2009@hotmail.fr>
2022-02-10 19:16:20 +0100

    trailing whitespace fixd-

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,13 +1,12 @@
 #include "main.h"
 
 /**
- * mul - multiplies two integers
- * @a: the first integer
- * @b: the second integer
- * Return: the integer result product
- */
+* mul - multiplies two integers
+* @a: the first integer
+* @b: the second integer
+* Return: the integer result product
+*/
 int mul(int a, int b)
 {
-	return (a * b);
-	
+return (a * b);
 }
