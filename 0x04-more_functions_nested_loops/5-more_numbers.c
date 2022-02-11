8056951e1f7e2b09e7af0c05563e65a05ed41eac
Siham <phenissia2009@hotmail.fr>
2022-02-11 01:06:21 +0100

    punition ;)

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,22 +1,23 @@
-# include "main.h"
+#include "main.h"
 
 /**
-* more_numbers - prints 10 times the numbers from 0 to 14
-* Return: void
-*/
+ * more_numbers - check the code punition
+ * Return: Always 0.
+ */
+
 void more_numbers(void)
 {
-int i, j; /* iteratrs */
-for (i = 0; i < 10; i++)
-{
-for (j = 0; j < 15; j++)
-{
-if (j > 10)
-{
-_putchar ((j / 10) + '0');
-}
-_putchar((j % 10) + '0');
-}
-_putchar('\n');
-}
+	int i;
+	int j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 15; j++)
+		{
+			if (j >= 10)
+				_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
+		}
+		_putchar('\n');
+	}
 }
