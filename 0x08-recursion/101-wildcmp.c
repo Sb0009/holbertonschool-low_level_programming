9b0620022a79b870c36004239318507a46819b0d
Siham <phenissia2009@hotmail.fr>
2022-02-24 23:52:12 +0100

    push success of the night

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,25 +1,23 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
-*/
-
-
+ * wildcmp - compares two strings and returns 1
+ * @s1: first string to compare
+ * @s2: second string to comp  wildcards
+ * Return: 1 if strings the same, else 0
+ */
 int wildcmp(char *s1, char *s2)
-
 {
-while (*s1 && *s2 && *s1 == *s2)
-{
-++s1;
-++s2;
-}
-return (1);
-}
-
-int wildcmp2(char *s1, char  *s2)
-
-{	  
-*s2 = ('*');	 
-if(s2 != 0 || !*s1)	   
-return (0);
-return (0);
+	if (!*s1)
+	{
+		if (*s2 == '*')
+		return (wildcmp(s1, s2 + 1));
+		return (!*s2);
+	}
+	if (*s2 == '*')
+		return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	return (0);
 }
