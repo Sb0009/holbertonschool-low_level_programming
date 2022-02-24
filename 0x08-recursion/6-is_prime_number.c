e8f631a2c1b887de33e2435da6bed118befc50d9
Siham <phenissia2009@hotmail.fr>
2022-02-24 19:02:49 +0100

    creation function

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ *prime_ornot - test if a number is divisible by i.
+ *@n: integer.
+ *@i: counter.
+ *Return: int.
+ */
+
+int prime_ornot(int i, int n)
+{
+	if (i == n)
+		return (1);
+	else if (n % i == 0)
+		return (0);
+	return (prime_ornot(i + 1, n));
+}
+
+/**
+ *is_prime_number - check if a number is prime.
+ *@n: integer.
+ *Return: int.
+ */
+
+int is_prime_number(int n)
+{
+	if (n < 2)
+		return (0);
+	return (prime_ornot(2, n));
+}
