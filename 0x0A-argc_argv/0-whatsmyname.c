7c0ef530a653567bb4752731161502189fadc450
Siham <phenissia2009@hotmail.fr>
2022-02-25 12:42:57 +0100

    Lancement du projet argc argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Whats my name
+ * @argc: number
+ * @argv: number
+ * Return: Always 0.
+ */
+
+
+
+int main(char *arg[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
