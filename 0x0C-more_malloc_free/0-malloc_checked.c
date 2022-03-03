e3e3ce5b37bd6018ad8f9310c1466d2161ad0ec5
siham <phenissia2009@hotmail.fr>
2022-03-03 20:02:51 +0100

    0. Trust no one

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ *malloc_checked - allocate memory
+ *@b: size  memory allocated
+ *Return: ptr;
+ **/
+
+void *malloc_checked(unsigned int b)
+{
+	char *p;
+	
+
+	p = malloc(b);
+	if (p == NULL)
+	{
+		exit(0);
+		return (NULL);
+	}
+	return (p);
+}
