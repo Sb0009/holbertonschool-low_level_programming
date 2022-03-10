235d2b25c12c8383f990b8d7b3082e625bffecc7
siham <phenissia2009@hotmail.fr>
2022-03-10 14:07:27 +0100

    Advanced Assembly remake

--- 0x10-variadic_functions/100-hello_world.asm
+++ 0x10-variadic_functions/100-hello_world.asm
@@ -1,15 +1,18 @@
-section .data
-    text db "Hello, World",10,0
-section .text
-    global main
- 
-main:
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, text
-    mov rdx, 17
-    syscall
+global _start
 
-    mov rax, 60
-    mov rdi, 0
-    syscall
+section .text
+
+_start:
+  mov rax, 1        ; write(
+  mov rdi, 1        ;   STDOUT_FILENO,
+  mov rsi, msg      ;   "Hello, world!\n",
+  mov rdx, msglen   ;   sizeof("Hello, world!\n")
+  syscall           ; );
+
+  mov rax, 60       ; exit(
+  mov rdi, 0        ;   EXIT_SUCCESS
+  syscall           ; );
+
+section .rodata
+  msg: db "Hello, world!", 10
+  msglen: equ $ - msg
