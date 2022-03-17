5e6bb1cc95c5a384e8e90f1b5618fe3feef69852
siham <phenissia2009@hotmail.fr>
2022-03-17 19:41:32 +0100

    print all

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -5,12 +5,19 @@
  * Return: counter
  */
 
+
 size_t print_list(const list_t *h)
 {
-	size_t count;
+	size_t count = 0;
 
-	for (count = 0 ; h; count++, h = h->next)
-		printf("%d\n", h->n);
+	for (count = 0; h != NULL; count++)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
 
+		h = h->next;
+	}
 	return (count);
 }
