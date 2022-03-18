df7c9966cbe0523e206105a113aee4b835b9c2fb
siham <phenissia2009@hotmail.fr>
2022-03-18 10:58:37 +0100

    lenght

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -8,12 +8,12 @@
 
 size_t list_len(const list_t *h)
 {
-	size_t node = 0;
+	size_t i = 0;
 
-while (h!= NULL)
+	while (h != NULL)
 	{
 		h = h->next;
-		len++;
+		n++;
 	}
-	return (len);
+return (i);
 }
