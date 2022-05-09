9b84aad22c2d00d2aafb137edd6a4fdba5039900
siham <phenissia2009@hotmail.fr>
2022-05-09 15:23:47 +0200

    lib

--- 0x18-dynamic_libraries/0-main.c
+++ 0x18-dynamic_libraries/0-main.c
@@ -1,13 +1,16 @@
 #include "holberton.h"
 #include <stdio.h>
 
+#include "main.h"
+#include <stdio.h>
+
 /**
- * main - check the code for Holberton School students.
+ * main - check the code
  *
  * Return: Always EXIT_SUCCESS.
  */
 int main(void)
 {
-    printf("%d\n", _strlen("Holberton"));
+    printf("%d\n", _strlen("My Dyn Lib"));
     return (EXIT_SUCCESS);
 }
